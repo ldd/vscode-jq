989f43b6caf6de3c21756b4ebf4007252e2a0edb
ldd <leonardoldd@gmail.com>
2019-12-29 03:18:28 -0500

    feat: add configuration options for jq command

--- src/commands/showPreview.ts
+++ src/commands/showPreview.ts
@@ -1,36 +1,37 @@
 import { Uri, ViewColumn, window, workspace } from "vscode";
-import { DEFAULT_COMMAND, runJQ, stringifyCommand } from "../jq";
+import { DEFAULT_FILTER, spawnJq, stringifyCommand } from "../jq";
 
 const showPreview = (queries: WeakMap<Uri, any>) => async (uri: Uri) => {
   if (!window.activeTextEditor) {
     return; // no editor
   }
   const {
-    document: { fileName, languageId, uri: documentUri }
+    document: { fileName, languageId, uri: documentUri },
   } = window.activeTextEditor;
   let jqCommand;
 
   if (languageId !== "json") {
     return;
   } // not a json file
+  const config = workspace.getConfiguration("jq");
 
   if (queries.has(uri)) {
     jqCommand = queries.get(uri);
   } else {
     jqCommand = await window.showInputBox({
-      placeHolder: DEFAULT_COMMAND
+      placeHolder: DEFAULT_FILTER,
     });
     queries.set(documentUri, jqCommand);
   }
-  const what = await runJQ(jqCommand, fileName);
-  if (what) {
+  const query = await spawnJq(jqCommand, fileName, config);
+  if (query) {
     const name = stringifyCommand(jqCommand);
-    const previewUri = Uri.parse(`jq:${name}.json`).with({ query: what });
+    const previewUri = Uri.parse(`jq:${name}.json`).with({ query });
     const doc = await workspace.openTextDocument(previewUri); // calls back into the provider
     await window.showTextDocument(doc, {
       preserveFocus: true,
       preview: true,
-      viewColumn: ViewColumn.Beside
+      viewColumn: ViewColumn.Beside,
     });
   }
 };
--- src/jq.ts
+++ src/jq.ts
@@ -1,17 +1,26 @@
 import { exec } from "child_process";
 import { promisify } from "util";
+import { WorkspaceConfiguration } from "vscode";
 
 const wrappedSpawn = promisify(exec);
 
 const DEFAULT_FILE = "sample.json";
 
-export const DEFAULT_COMMAND = "map(select(.craftable) | {name, frame})";
+export const DEFAULT_FILTER = ".";
 
-export async function runJQ(
-  command: string = DEFAULT_COMMAND,
-  file: string = DEFAULT_FILE
+export async function spawnJq(
+  userFilter: string = DEFAULT_FILTER,
+  filePath: string = DEFAULT_FILE,
+  config: WorkspaceConfiguration
 ) {
-  const { stdout } = await wrappedSpawn(`jq '${command}' ${file}`);
+  // VSCode extensions currently do not support variable resolution in settings
+  // https://github.com/microsoft/vscode/issues/2809
+  // to work around it, we use well-defined variables starting with $$ that we replace here
+  const { customCommand = `jq '$$user_filter' $$file_path` } = config;
+  const parsedCommand = customCommand
+    .replace("$$user_filter", userFilter)
+    .replace("$$file_path", filePath);
+  const { stdout } = await wrappedSpawn(parsedCommand);
   return stdout;
 }
 
